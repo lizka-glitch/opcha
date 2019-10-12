62ec5eb922fc45ed98fa3a590b3aa33977221a20
lizka-glitch <55316931+lizka-glitch@users.noreply.github.com>
2019-10-12 13:19:49 +0300

    вввв

---
+++ лаба/лаба/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace лаба
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("пиздец!");
+        }
+    }
+}
